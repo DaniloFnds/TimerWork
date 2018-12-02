28b005f72333645efcd83a652b64c19897e6c47e
Danilo Fernandes <DaniloFnds@users.noreply.github.com>
2018-12-02 02:37:29 -0200

    esgdfgsd

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/timerwork'));
+app.use(express.static(__dirname + '/dist/TimerWork'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/timerwork/index.html'));
+res.sendFile(path.join(__dirname+'/dist/TimerWork/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
